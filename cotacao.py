264d029caa4b3235003007cd5c7009c50ab4f06b
Vitor Barbosa <62035588+vitwb@users.noreply.github.com>
2020-07-14 10:53:31 -0300

    Update cotacao.py

--- cotacao.py
+++ cotacao.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 import time
 import os
 
-
 class cotacao:
     def __init__(self):
         self.driver = webdriver.Firefox(executable_path=r'D:\Biblioteca Eng_WB\Downloads\geckodriver-v0.26.0-win64\geckodriver.exe')
@@ -14,7 +13,6 @@ class cotacao:
         user_element = self.driver.find_element_by_xpath('/html/body/div[2]/div[4]/div/header/div/div[3]/div[1]/div/div/div/div[1]/div[1]').text
         return user_element
         
-
 valor=cotacao()
 while True:
     preco = valor.preco()
